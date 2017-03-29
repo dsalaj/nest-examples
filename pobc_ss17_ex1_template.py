c22fe62a091729a81bf471bce667f626948e364a
Darjan Salaj <salaj@gmx.at>
2017-03-29 15:30:18 +0200

    most of ex1 done

--- pobc_ss17_ex1_template.py
+++ pobc_ss17_ex1_template.py
@@ -6,26 +6,28 @@
 #   A leaky integrate-and-fire neuron is created.
 #   It gets a spike input at time t=55 ms 
 #   There is also an optional step current input at time t=50 ms that forces the neuron to spike
-#   
+#   There is also an optional step current input at time t=50 ms that forces the neuron to spike
+#
 #
 #
 #*******************************************************************
 # At the beginning we import the necessary Python packages
 from numpy import *       # for numerical operations
-from pylab import *       # for plotting (matplotlib)
+import pylab       # for plotting (matplotlib)
 import nest
 import nest.voltage_trace
+import nest.raster_plot
 
-close('all');
+pylab.close('all')
 
 
 ###########################################
 #  Parameter for the LIF neuron
 ###########################################
-Rm = 10.0 # [MOhms]
-Cm = 2000. # [pF]
+Rm = 10.0  # [MOhms]
+Cm = 2000.  # [pF]
 tau_m = Rm*Cm/1000.0  # membrane time constant [ms]
-tau_s = 10.0;     # synaptic time constant [ms]
+tau_s = 10.0     # synaptic time constant [ms]
 Trefract = 10.   # The refractory period of the LIF neuron [ms]
 Vthresh = -45.   # The threshold potential of the LIF neuron [mV]
 Vresting = -60.  # The resting potential of the LIF neuron [mV]
@@ -50,34 +52,50 @@ nest.ResetKernel()
 ###################################
 # Create nodes
 ###################################
-    
+
 # Create the IAF neuron, see http://www.nest-simulator.org/cc/iaf_psc_exp/
+neuron = nest.Create('iaf_psc_exp', 1, nrn_parameter_dict)
 
 # Create inputs
-spike_gen = nest.Create("spike_generator", params = {"spike_times": array([t_spike_input])})
+t_spike_input = 55.
+t_step = 50.
+step_duration = 0.5
+step_amplitude = 60790.
+# step_amplitude = 1.
+
+#sine = nest.Create('ac_generator',1,{'amplitude':100.0,'frequency':2.0})
+spike_gen = nest.Create("spike_generator", params={"spike_times": array([t_spike_input])})
 step_gen = nest.Create('step_current_generator')
-nest.SetStatus(step_gen,{'amplitude_times':array([t_step,t_step+step_duration]),'amplitude_values':array([step_amplitude, 0.])})
+nest.SetStatus(step_gen, {'amplitude_times': array([t_step, t_step+step_duration]),
+                          'amplitude_values': array([step_amplitude, 0.])})
 # step_amplitude is in [pA]
 # needs a large value to produce a spike
-    
+
 # Create voltmeter and spike recorder
+voltmeter = nest.Create('voltmeter', 1, {'withgid': True})
+spike_rec = nest.Create('spike_detector')
 
 ###################################
 # Connect nodes
 ###################################
-     
+
+#nest.Connect(sine,neuron)
 # Connect spike generator to neuron
 # Note: The connection weight is given in [pA]
+nest.Connect(spike_gen, neuron, syn_spec={'delay': 2.0})
 
 # Connect current step input step_gen to the neuron
 # Note: The current amplitudes as defined above are multiplied with the weight.
+nest.Connect(step_gen, neuron, syn_spec={'delay': 2.0})
 
 # Connect voltmeter and spike recorder to neuron
+nest.Connect(voltmeter, neuron)
+nest.Connect(neuron, spike_rec)
 
 ###################################
 # Now simulate 
 ###################################
-    
+nest.Simulate(500.0)
 
 ###################################
 # Analyze results and make plots
@@ -86,6 +104,13 @@ nest.SetStatus(step_gen,{'amplitude_times':array([t_step,t_step+step_duration]),
 
 # Extract spikes voltage
 spikes = nest.GetStatus(spike_rec)[0]['events']['times']
-vm = nest.GetStatus(voltmeter,'events')[0]['V_m']
+print(spikes)
+# events = nest.GetStatus(spike_rec, 'events')
+vm = nest.GetStatus(voltmeter, 'events')[0]['V_m']
 
 # etc.
+# Plot results
+nest.voltage_trace.from_device(voltmeter)
+pylab.show()
+# nest.raster_plot.from_device(spike_rec)
+# pylab.show()
