52c01f69243b1eb55e3d4d6a2500be8888d0afc3
Darjan Salaj <salaj@gmx.at>
2017-05-06 11:54:39 +0200

    Fixed parameter types and jitter

--- ex3_stdp.py
+++ ex3_stdp.py
@@ -11,6 +11,8 @@ import nest
 from numpy import *       # for numerical operations
 from pylab import *       # for plotting (matplotlib)
 from pobc_utils import *       # for generating poisson spike trains
+import nest.raster_plot
+import nest.voltage_trace
 
 nest.set_verbosity("M_WARNING") # surpress too much text output
 
@@ -74,7 +76,7 @@ def construct_input_population(Nin, jitter, Tsim, sequence):
         else:
            inp_spikes, s_occur = generate_stimulus(int(Nin/2), 2.0, jitter, 8.0, Tsim)
         inp_spikes_2, s_occur_2 = generate_stimulus(int(Nin/2), 0.0, 0e-3, 8.0, Tsim)
-        
+
         inp_spikes += inp_spikes_2
         
         spike_generators = nest.Create("spike_generator", Nin)
@@ -118,33 +120,34 @@ def perform_simulation(sequence, jitter=0.0, alpha=1.1, Wmax_fact=2, Tsim=200.0,
     #########################################
     syn_param = {"weight": 2 * 1e3,
                  "lambda": 0.005,
-                 "tau_plus": 30,
+                 "tau_plus": 30.,
                  "alpha": alpha,
                  "Wmax": Wmax_fact * W,
-                 "mu_plus": 0,
-                 "mu_minus": 0
+                 "mu_plus": 0.,
+                 "mu_minus": 0.
                  }
     nest.CopyModel("stdp_synapse", "syn", syn_param)
 
-    Rm = 1
-    Cm = 30e3
+    Rm = 1.
+    Cm = 30.e3
     tau_m = Rm * Cm / 1000.0
     lif_params = {#"V_m": -60.,  # Membrane potential in mV
                   "E_L": -60.,  # Resting membrane potential in mV
                   "C_m": Cm,  # Capacity of the membrane in pF
                   "tau_m": tau_m,  # Membrane time constant in ms
                   "V_th": -45.,  # Spike threshold in mV
-                  "V_reset": -60,  # Reset potential of the membrane in mV
+                  "V_reset": -60.,  # Reset potential of the membrane in mV
                   "t_ref": 2.  # refractory time in ms
                   }
     lif_neuron = nest.Create("iaf_psc_exp", 1)
     nest.SetStatus(lif_neuron, lif_params)
     voltmeter = nest.Create('voltmeter', 1, {'withgid': True})
     spike_rec = nest.Create('spike_detector')
+    in_spike_rec = nest.Create('spike_detector')
 
 
     # the follwoing creates N input neurons and sets their spike trains during simulation
-    spike_generators,input_neurons = construct_input_population(N, jitter, Tsim, sequence)
+    spike_generators, input_neurons = construct_input_population(N, jitter, Tsim, sequence)
 
     #########################################
     # Connect nodes, simulate
@@ -152,23 +155,29 @@ def perform_simulation(sequence, jitter=0.0, alpha=1.1, Wmax_fact=2, Tsim=200.0,
     nest.Connect(input_neurons, lif_neuron, syn_spec={"model": "syn"})
 
     # connect recorders
-    nest.Connect(voltmeter, lif_neuron)
+    # nest.Connect(voltmeter, input_neurons)
     nest.Connect(lif_neuron, spike_rec)
+    nest.Connect(input_neurons, in_spike_rec)
+    # nest.Connect(spike_generators, in_spike_rec)
 
     weight_evolution = []
-    synapses = nest.GetConnections(input_neurons, lif_neuron)
     for i in range(int(Tsim)):
         nest.Simulate(1 * 1000.0)
+        synapses = nest.GetConnections(input_neurons, lif_neuron)
         weight_evolution.append(nest.GetStatus(synapses, "weight"))
     weight_evolution = np.array(weight_evolution)
 
     # To extract spikes of input neuons as a list of numpy-arrays, use the
     # following function provided in nnb_utils:
-    spikes_in = get_spike_times(spike_rec)
+    spikes_in = get_spike_times(in_spike_rec)
 
     # extract spike times and convert to [s]
     events = nest.GetStatus(spike_rec, 'events')
     spikes = events[0]['times']
+    # nest.raster_plot.from_device(in_spike_rec)
+    # nest.voltage_trace.from_device(voltmeter)
+    # show()
+    # exit(1)
 
     return spikes, weight_evolution, spikes_in
 
@@ -284,5 +293,5 @@ def plot_figures(fig1,fig2, spikes, weights, inp_spikes, Tsim, filename_fig1, fi
     savefig(filename_fig2)
 
 
-spikes, weight_evolution, spikes_in = perform_simulation(sequence=False, jitter=2, alpha=1.1)
+spikes, weight_evolution, spikes_in = perform_simulation(sequence=False, jitter=.002, alpha=1.1, Tsim=200.)
 plot_figures(0, 1, spikes, weight_evolution, spikes_in, 200., filename_fig1="fig1", filename_fig2="fig2")
